3e043e7ad5a2a53b50737275d9616d5946e46605
Yonanda Putra <93269134+yonand21@users.noreply.github.com>
2023-06-17 15:02:31 +0800

    Update index.js route add

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,17 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
+import App from './views/app';
 
+const app = new App({
+    button: document.querySelector('#hamburgerButton'),
+    drawer: document.querySelector('#navigationDrawer'),
+    content: document.querySelector('#mainContent'),
+  });
 
-console.log('Hello Coders! :)');
+  window.addEventListener('hashchange', () => {
+    app.renderPage();
+  });
+  
+  window.addEventListener('load', async () => {
+    app.renderPage();});
