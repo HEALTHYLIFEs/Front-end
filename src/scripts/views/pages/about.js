768177a9b8220034eb8c0c6d5c40e75dc4c13a8a
Hilma Nur Kholilah <134673766+HilmaNurKh@users.noreply.github.com>
2023-06-17 16:27:31 +0700

    Update about.js
    about us

--- src/scripts/views/pages/about.js
+++ src/scripts/views/pages/about.js
@@ -1,11 +1,71 @@
 const About = {
     async render() {
       return `
-      <div class="content">
-        <h2 tabindex="0" class="content-heading">Daftar Restoran Favorite</h2>
-        <div id="resto-list" class="resto-list"></div>
-      </div>
-      `;
+      <div class="section-title">
+                          <h2>MEET OUR TEAM</h2>
+                          <h3>C23-M4090</h3>
+                      </div>
+        
+              <div class="row">
+    
+                  <div class="col-md-3 col-sm-6">
+                      <div class="team-item">
+                          <div class="item-image">
+                              <img src="./images/team/hilma.jpg" alt="Hilma Nur Kholilah" width="100%">
+                          </div>
+                          <div class="item-meta">
+                              <h3 class="item-title">Hilma Nur Kholilah</h3>
+                              <h6 class="item-position">STMIK Bandung</h6>
+                          </div>
+                      </div>
+                  </div>
+                
+                  <div class="col-md-3 col-sm-6">
+                      <div class="team-item">
+                          <div class="item-image">
+                              <img src="./images/team/tuti.jpg" alt="Tuti Alawiyah" width="100%">
+                          </div>
+                          <div class="item-meta">
+                              <h3 class="item-title">Tuti Alawiyah</h3>
+                              <h6 class="item-position">STMIK Bandung</h6>
+                          </div>
+                      </div>
+                  </div>
+                
+                  <div class="col-md-3 col-sm-6">
+                      <div class="team-item">
+                          <div class="item-image">
+                              <img src="./images/team/wahyu.jpg" alt="" width="100%">
+                          </div>
+                          <div class="item-meta">
+                              <h3 class="item-title">Wahyu Andi Saputra</h3>
+                              <h6 class="item-position">Universitas Negeri Semarang</h6>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="col-md-3 col-sm-6">
+                      <div class="team-item">
+                          <div class="item-image">
+                              <img src="./images/yonda.png" alt="" width="100%">
+                          </div>
+                          <div class="item-meta">
+                              <h3 class="item-title">Yonanda Putra Pamungkas</h3>
+                              <h6 class="item-position">Universitas Pendidikan Ghanesa</h6>
+                          </div>
+                      </div>
+                  </div>
+    
+              </div>
+          </div>
+      </section>
+      
+    
+            `;
+    },
+  
+    async afterRender() {
+      return this.render;
     },
   };
   
