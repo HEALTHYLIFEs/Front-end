e5832be4ee66b3aa9499c40fbea335fc0542085b
Hilma Nur Kholilah <134673766+HilmaNurKh@users.noreply.github.com>
2023-06-17 16:38:49 +0700

    Update about.js
    update pertama

--- src/scripts/views/pages/about.js
+++ src/scripts/views/pages/about.js
@@ -6,55 +6,47 @@ const About = {
                           <h3>C23-M4090</h3>
                       </div>
         
-              <div class="row">
-    
-                  <div class="col-md-3 col-sm-6">
-                      <div class="team-item">
-                          <div class="item-image">
+              <div class="container">
+  
+                      <div class="card">
+                          <div class="card-img">
                               <img src="./images/team/hilma.jpg" alt="Hilma Nur Kholilah" width="100%">
                           </div>
                           <div class="item-meta">
-                              <h3 class="item-title">Hilma Nur Kholilah</h3>
+                              <h3 class="card-title">Hilma Nur Kholilah</h3>
                               <h6 class="item-position">STMIK Bandung</h6>
                           </div>
-                      </div>
-                  </div>
+                      </div
                 
-                  <div class="col-md-3 col-sm-6">
-                      <div class="team-item">
-                          <div class="item-image">
+                      <div class="card">
+                          <div class="card-img">
                               <img src="./images/team/tuti.jpg" alt="Tuti Alawiyah" width="100%">
                           </div>
-                          <div class="item-meta">
-                              <h3 class="item-title">Tuti Alawiyah</h3>
+                          <div class="card-meta">
+                              <h3 class="card-title">Tuti Alawiyah</h3>
                               <h6 class="item-position">STMIK Bandung</h6>
                           </div>
                       </div>
-                  </div>
                 
-                  <div class="col-md-3 col-sm-6">
-                      <div class="team-item">
-                          <div class="item-image">
+                      <div class="card">
+                          <div class="card-img">
                               <img src="./images/team/wahyu.jpg" alt="" width="100%">
                           </div>
                           <div class="item-meta">
-                              <h3 class="item-title">Wahyu Andi Saputra</h3>
+                              <h3 class="card-title">Wahyu Andi Saputra</h3>
                               <h6 class="item-position">Universitas Negeri Semarang</h6>
                           </div>
                       </div>
-                  </div>
-
-                  <div class="col-md-3 col-sm-6">
-                      <div class="team-item">
-                          <div class="item-image">
+                      
+                      <div class="card">
+                          <div class="card-img">
                               <img src="./images/yonda.png" alt="" width="100%">
                           </div>
                           <div class="item-meta">
-                              <h3 class="item-title">Yonanda Putra Pamungkas</h3>
+                              <h3 class="card-title">Yonanda Putra Pamungkas</h3>
                               <h6 class="item-position">Universitas Pendidikan Ghanesa</h6>
                           </div>
                       </div>
-                  </div>
     
               </div>
           </div>
