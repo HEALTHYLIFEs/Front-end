50193b2c5c0c36db25bddf4ce7b5804168498015
Hilma Nur Kholilah <134673766+HilmaNurKh@users.noreply.github.com>
2023-06-19 14:13:05 +0700

    Update about.js page
    update about.js 
    add about team
    team solved

--- src/scripts/views/pages/about.js
+++ src/scripts/views/pages/about.js
@@ -1,6 +1,49 @@
 const About = {
     async render() {
       return `
+        <div class="about-title">
+        <h1>ABOUT PAGE</h1>
+        </div>
+
+        <div class="container1">
+            <div class="purpose">
+                <h2>PURPOSE</h2>
+                <p> HEALTHYLIFE adalah website yang memiliki tujuan untuk menyajikan informasi seputar kesehatan, pola hidup sehat dan olah raga. 
+                Website ini berfungsi sebagai sumber rujukan yang komprehensif bagi pengunjung yang ingin belajar atau mencari informasi. </p>
+            <div class="description">
+                <h2>BACKGROUND</h2>
+                <p>
+                Apa pentingnya pola hidup sehat bagi tubuh? Pola hidup sehat adalah salah satu aspek 
+                penting sebagai penunjang aktivitas. Lalu, bagaimana situasi pola hidup sehat saat ini? Di era 
+                modern saat ini, manusia dapat dengan mudahnya penyelesaian tugas dengan bantuan 
+                teknologi yang menjadi penyebab lalainya manusia menerapkan hidup sehat. Saat ini, gaya 
+                hidup modern menyuapi kita dengan hal-hal yang praktis. 
+                </p>
+                <p>
+                Dengan segala kemudahan itu, membuat kita lupa bahwa pola hidup sehat dan gaya hidup 
+                yang baik adalah faktor penting dalam meningkatkan kualitas hidup. Dengan kemajuan 
+                teknologi saat ini, apakah perkembangan teknologi mempengaruhi kesehatan manusia? 
+                Teknologi yang semakin maju tanpa ada batasan tentu saja memberikan dampak positif dan 
+                negatif secara langsung maupun tidak langsung.
+                </p>
+                <p>
+                Positifnya, dengan kemajuan teknologi bisa tercipta alat kesehatan yang lebih canggih, 
+                mumpuni, dan kredibel. Selain itu, informasi mengenai kesehatan lebih mudah di akses. 
+                Dampak negatifnya, manusia bisa terlalu sibuk dengan urusan pekerjaan yang praktis 
+                sehingga tidak punya waktu untuk memperhatikan kesehatannya. Dengan latar belakang itu 
+                lah, kami bertujuan untuk membuat project yang bisa menunjang kebutuhan masyarakat saat 
+                ini dengan memanfaatkan kemajuan teknologi. 
+                </p>
+                </div>
+            <div class="description">
+                <h2>PROFILE</h2>
+                <p>Dibuat dan dikembangkan pada bulan Mei 2023 oleh tim C23-M4090</p>
+                </div>
+            </div>
+     </div>
+
+
+
       <div class="section-title">
                           <h2>MEET OUR TEAM</h2>
                           <h3>C23-M4090</h3>
@@ -14,11 +57,11 @@ const About = {
                                     <p>Front-end Developer</p>
                                     </div>
 
-                                    <!-- <div class="social-icons">
-                                    <a href="https://github.com/HilmaNurKh"><img src="./icons/logo/git.png" alt="GitHub"></a>
-                                    <a href="https://linkedin.com/in/hilma-nur-kholilah-927558275"><img src="./icons/logo/linkedin.png" alt="LinkedIn"></a>
-                                    <a href="https://instagram.com/hilmaaaa.nkh/"><img src="./icons/logo/ig.png" alt="Instagram"></a>
-                                    </div> --!>
+                                    <div class="social-icons">
+                                    <a href="https://www.instagram.com/hilmaaaa.nkh"><i class="fab fa-instagram"></i></a>
+                                    <a href="https://github.com/HilmaNurKh"><i class="fab fa-github"></i></a>
+                                    <a href="https://www.linkedin.com/in/hilma-nur-kholilah-927558275"><i class="fab fa-linkedin"></i></a>
+                                    </div>
                         </div>
                         
                         <div class="card">
@@ -28,11 +71,11 @@ const About = {
                                     <p>Front-end Developer</p>
                                     </div>
 
-                                    <!-- <div class="social-icons">
-                                    <a href="https://github.com/TutiAlwyh"><img src="./icons/logo/git.png" alt="GitHub"></a>
-                                    <a href="https://linkedin.com/in/tutialwyh"><img src="./icons/logo/linkedin.png" alt="LinkedIn"></a>
-                                    <a href="https://instagram.com/hilmaaaa.nkh/"><img src="./icons/logo/ig.png" alt="Instagram"></a>
-                                    </div> --!>
+                                    <div class="social-icons">
+                                    <a href="https://www.instagram.com/2ty_alawiyah"><i class="fab fa-instagram"></i></a>
+                                    <a href="https://github.com/TutiAlwyh"><i class="fab fa-github"></i></a>
+                                    <a href="https://www.linkedin.com/in/tutialwyh"><i class="fab fa-linkedin"></i></a>
+                                    </div>
                         </div>
             
                         <div class="card">
@@ -42,25 +85,26 @@ const About = {
                                     <p>Front-end Developer</p>
                                     </div>
 
-                                    <!-- <div class="social-icons">
-                                    <a href="https://github.com/Wahyuandii"><img src="./icons/logo/git.png" alt="GitHub"></a>
-                                    <a href="https://linkedin.com/in/wahyu-andi-saputra-598ab8278"><img src="./icons/logo/linkedin.png" alt="LinkedIn"></a>
-                                    <a href="https://instagram.com/hilmaaaa.nkh/"><img src="./icons/logo/ig.png" alt="Instagram"></a>
-                                    </div> --!>
+                                    <div class="social-icons">
+                                    <a href="https://www.instagram.com/andiii._"><i class="fab fa-instagram"></i></a>
+                                    <a href="https://github.com/Wahyuandii"><i class="fab fa-github"></i></a>
+                                    <a href="https://www.linkedin.com/in/wahyu-andi-saputra-598ab8278"><i class="fab fa-linkedin"></i></a>
+                                    </div>
                         </div>
 
                         <div class="card">
                             <img src="./images/team/yonanda.png" alt="Yonanda Putra Pamungkas" class="card-image">
                                 <div class="card-content">
-                                    <h2>Yonanda Putra Pamungkas</h2>
+                                    <h2>Yonanda Putra P.</h2>
                                     <p>Back-end Developer</p>
                                     </div>
 
-                                    <!-- <div class="social-icons">
-                                    <a href="https://github.com/yonand21"><img src="./icons/logo/git.png" alt="GitHub"></a>
-                                    <a href="https://linkedin.com/in/yonanda-putra-pamungkas"><img src="./icons/logo/linkedin.png" alt="LinkedIn"></a>
-                                    <a href="https://instagram.com/hilmaaaa.nkh/"><img src="./icons/logo/ig.png" alt="Instagram"></a>
-                                    </div> --!>
+                                    <div class="social-icons">
+                                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                                    <a href="https://github.com/yonand21"><i class="fab fa-github"></i></a>
+                                    <a href="https://www.linkedin.com/in/yonanda-putra-pamungkas"><i class="fab fa-linkedin"></i></a>
+                                    </div>
+
                         </div>
 
                         </div>
